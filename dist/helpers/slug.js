f98b25b89502f17c166a5fde4631453866f87059
Techhodu <itssocialsk@gmail.com>
2024-09-29 08:23:22 +0530

    data base fix

--- dist/helpers/slug.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slugBuilder = void 0;
-const slugBuilder = async (e) => {
-    let tempSlug = await e.replace(/[`~!@#$%^&*()_|+\=?;:'",.<>\{\}\[\]\\\/]/gi, "");
-    tempSlug = await tempSlug.replace(/ /g, "-").toLowerCase();
-    return tempSlug;
-};
-exports.slugBuilder = slugBuilder;
